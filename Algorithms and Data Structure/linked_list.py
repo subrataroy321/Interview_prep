7ec8095e0599cae6566c96fe3318d0be661af288
SUBRATA ROY <subrata.r321@gmail.com>
2021-04-24 15:27:15 -0400

    fix indentation of linked list

--- Algorithms and Data Structure/linked_list.py
+++ Algorithms and Data Structure/linked_list.py
@@ -12,26 +12,26 @@ class Node:
     def __repr__(self):
         return "<Node data: %s>" % self.data
 
-    class LinkedList:
+class LinkedList:
+    """
+    Singly linked list
+    """
+
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        return self.head == None
+
+    def size(self):
         """
-        Singly linked list
+        Returns the number of nodes in the list
+        Takes O(n) time
         """
+        current = self.head
+        count = 0
 
-        def __init__(self):
-            self.head = None
-
-        def is_empty(self):
-            return self.head == None
-
-        def size(self):
-            """
-            Returns the number of nodes in the list
-            Takes O(n) time
-            """
-            current = self.head
-            count = 0
-
-            while current:
-                count += 1
-                current = current.next_node
-            return count
+        while current:
+            count += 1
+            current = current.next_node
+        return count
