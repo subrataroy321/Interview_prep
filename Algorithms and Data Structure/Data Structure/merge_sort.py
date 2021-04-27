5f1b21d9f937704923562bd668c73bf7d0197b13
SUBRATA ROY <subrata.r321@gmail.com>
2021-04-27 15:21:26 -0400

    add Time complexity for each function and overall time complexity, and its so much expensive

--- Algorithms and Data Structure/Data Structure/merge_sort.py
+++ Algorithms and Data Structure/Data Structure/merge_sort.py
@@ -6,6 +6,8 @@ def merge_sort(list):
     Divide: Find the midpoint of the list and devide into sublists
     Conquer: Recursively sort the sublists created in previous step
     Combine: Merge the sorted sublists created in previous step
+
+    Takes O(kn log n) time
     """
 
     if len(list) <= 1:
@@ -21,6 +23,8 @@ def split(list):
     """
     Divide the unsorted list at midpoint into sublists
     Returns two sublists - left and right
+
+    Takes Overall O(k log n) time
     """
 
     mid = len(list)//2
@@ -33,6 +37,8 @@ def merge(left, right):
     """
     Merges two lists, sorting them in the process 
     Returns a new merge list
+
+    Runs in overall O(n) time
     """
     l = []
     i = 0
