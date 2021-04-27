fb0eafbf3dcaeb77b084fbe3e23ffbe25d255af4
SUBRATA ROY <subrata.r321@gmail.com>
2021-04-27 15:15:15 -0400

    create different repos for algorithm and data structure, add merge_sort function and verify sorted function to verify the list is sorted

---
+++ Algorithms and Data Structure/Data Structure/merge_sort.py
@@ -0,0 +1,71 @@
+def merge_sort(list):
+    """
+    Sorts a list in a asending order
+    Returns a new sorted list
+
+    Divide: Find the midpoint of the list and devide into sublists
+    Conquer: Recursively sort the sublists created in previous step
+    Combine: Merge the sorted sublists created in previous step
+    """
+
+    if len(list) <= 1:
+        return list
+    
+    left_half, right_half = split(list)
+    left = merge_sort(left_half)
+    right = merge_sort(right_half)
+
+    return merge(left, right)
+
+def split(list):
+    """
+    Divide the unsorted list at midpoint into sublists
+    Returns two sublists - left and right
+    """
+
+    mid = len(list)//2
+    left = list[:mid]
+    right = list[mid:]
+
+    return left, right
+
+def merge(left, right):
+    """
+    Merges two lists, sorting them in the process 
+    Returns a new merge list
+    """
+    l = []
+    i = 0
+    j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            l.append(left[i])
+            i += 1
+        else:
+            l.append(right[j])
+            j += 1
+
+    while i < len(left):
+        l.append(left[i])
+        i += 1
+
+    while j < len(right):
+        l.append(right[j])
+        j += 1
+    return l
+
+def verify_sorted(list):
+    n = len(list)
+
+    if n == 0 or n == 1:
+        return True
+
+    return list[0] < list[1] and verify_sorted(list[1:])
+
+
+
+alist = [54, 26, 93, 17, 77, 31, 67, 34, 11, 2, 99, 202, 151, 555]
+l = merge_sort(alist)
+print(verify_sorted(alist))
+print(verify_sorted(l))
