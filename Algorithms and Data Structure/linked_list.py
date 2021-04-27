cd8b17954542b1b3a3dc87edb3995156b0c57975
SUBRATA ROY <subrata.r321@gmail.com>
2021-04-26 23:49:29 -0400

    add repr function for linked list

--- Algorithms and Data Structure/linked_list.py
+++ Algorithms and Data Structure/linked_list.py
@@ -44,3 +44,22 @@ class LinkedList:
         new_node = Node(data)
         new_node.next_node = self.head
         self.head = new_node
+
+    def __repr__(self):
+        """
+        Return a string representaion of the list
+        takes O(n) time
+        """
+        nodes = []
+        current = self.head
+
+        while current:
+            if current is self.head:
+                nodes.append("[Head: %s]" %current.data)
+            elif current.next_node is None:
+                nodes.append("[Tail: %s]" %current.data)
+            else:
+                nodes.append("[%s]" %current.data)
+
+            current = current.next_node
+        return ' -> '.join(nodes)
