4bb3ca6fac87a6a752f0f26db1e83f3d2a3969f8
SUBRATA ROY <subrata.r321@gmail.com>
2021-04-27 00:38:14 -0400

    add insert and remove method for linked list

--- Algorithms and Data Structure/linked_list.py
+++ Algorithms and Data Structure/linked_list.py
@@ -61,6 +61,56 @@ class LinkedList:
                 current = current.next_node
         return None
 
+    def insert(self, data, index):
+        """
+        Inserts a new Node containing data at index position
+        Insertaion takes O(1) time but funding the node at the insertion point takes O(n) time.
+
+        Overall takes O(n) time
+        """
+        if index == 0:
+            self.add(data)
+
+        if index > 0:
+            new = Node(data)
+
+            position = index
+            current = self.head
+
+            while position > 1:
+                current = new.next_node
+                position -= 1
+            prev_node = current
+            next_node = current.next_node
+
+            prev_node.next_node = new
+            new.next_node = next_node
+
+    def remove(self, key):
+        """
+        Removes node containing data that matches the key
+        Returns the node or None if key doesn't exist
+
+        Takes O(n) times 
+        """
+        current = self.head
+        previous = None
+        found = False
+
+        while current and not found:
+            if current.data == key and current is self.head:
+                found = True
+                self.head = current.next_node
+            elif current.data == key:
+                found = True
+                previous.next_node = current.next_node
+            else:
+                previous = current
+                current = current.next_node
+
+        return current
+
+
     def __repr__(self):
         """
         Return a string representaion of the list
